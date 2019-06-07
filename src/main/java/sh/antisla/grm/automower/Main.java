d2ce08d5d816141bd3b8c59244c1673e2444ab1b
Grm <grm.klein@gmail.com>
2019-06-07 10:41:42 +0200

    Add main to test travis and codeclimate

---
+++ src/main/java/sh/antisla/grm/automower/Main.java
@@ -0,0 +1,16 @@
+package sh.antisla.grm.automower;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.debug("Main.class");
+        logger.info("Main.class");
+        logger.warn("Main.class");
+        logger.error("Main.class");
+    }
+}
